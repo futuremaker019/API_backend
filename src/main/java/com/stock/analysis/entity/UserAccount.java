9a6e4692e457024d031ca796961523255ccf6425
futuremaker019 <futuremaker019@gmail.com>
2023-10-23 13:23:47 +0900

    UserAccount Entity 추가

---
+++ src/main/java/com/stock/analysis/entity/UserAccount.java
@@ -0,0 +1,45 @@
+package com.stock.analysis.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@ToString(callSuper = true)
+@Table(indexes = {
+        @Index(columnList = "email", unique = true),
+})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @Column(nullable = false)
+    private String userId;
+
+    @Setter
+    @Column(length = 100)
+    private String email;
+
+    @Setter
+    @Column(length = 100)
+    private String nickname;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserAccount userAccount)) return false;
+        return this.getId() != null && this.getId().equals(userAccount.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getUserId());
+    }
+}
