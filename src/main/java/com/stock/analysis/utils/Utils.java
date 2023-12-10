b53231dd64cb188ca3c240f5a7c1fcc5aa1d8dd2
Goohyun Jung <futuremaker019@gmail.com>
2023-12-10 23:51:22 +0900

    #4 - 날짜변환을 위한 Util class 생성

---
+++ src/main/java/com/stock/analysis/utils/Utils.java
@@ -0,0 +1,13 @@
+package com.stock.analysis.utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Utils {
+
+    /**
+     * TODO: dataTime 형식으로 변환하여 화면에 보내줘야 함
+     * @Bean 으로 할지 싱글턴으로 할지 고민중
+     */
+
+}
