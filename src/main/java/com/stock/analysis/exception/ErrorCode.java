b2ecc9165a931e2f5c4c0874bc3df226533ce32c
futuremaker <futuremaker019@gmail.com>
2024-02-25 12:15:09 +0900

    #10 - 코드관리/에러코드 상태값 주석으로 추가

--- src/main/java/com/stock/analysis/exception/ErrorCode.java
+++ src/main/java/com/stock/analysis/exception/ErrorCode.java
@@ -11,23 +11,21 @@ public enum ErrorCode {
     /**
      * 프론트에서 에러구분을 httpstatus와 enum명으로 구분하기 때문에 수정시 고려하자 - httpstatus와 enum명 중 하나만 고려하면 될거 같긴하다
      */
-    
-    //Authentication
-    HAS_NO_ARGUMENTS(HttpStatus.BAD_REQUEST, "has no Arguments"),           // 인풋의 데이터가 없을떄
+    HAS_NO_ARGUMENTS(HttpStatus.BAD_REQUEST, "has no Arguments"),           // 400 인풋의 데이터가 없을떄
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found"),
     USER_EXISTED(HttpStatus.CONFLICT, "User Existed"),
 
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid token"),
-    REFRESH_TOKEN_NOT_FOUND(HttpStatus.GONE, "Invalid refresh token"),
+    //Authentication
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid token"),                // 401
+    REFRESH_TOKEN_NOT_FOUND(HttpStatus.GONE, "Invalid refresh token"),      // 410
     EXPIRED_TOKEN(HttpStatus.GONE, "token expired"),
 
-    HAS_NO_AUTHORITIES(HttpStatus.FORBIDDEN, "Has no Authorities"),
+    HAS_NO_AUTHORITIES(HttpStatus.FORBIDDEN, "Has no Authorities"),         // 403
     PASSWORD_NOT_MATCHED(HttpStatus.NOT_FOUND, "Password not matched"),
 
-    CODE_NOT_FOUND(HttpStatus.NOT_FOUND, "Code not found"),
-    CODE_NAME_EXISTED(HttpStatus.CONFLICT, "code name existed"),
-
-    CODE_CHILDREN_EXISTED(HttpStatus.CONFLICT, "children existed")
+    CODE_NOT_FOUND(HttpStatus.NOT_FOUND, "Code not found"),                 // 404
+    CODE_NAME_EXISTED(HttpStatus.CONFLICT, "code name existed"),            // 409
+    CODE_CHILDREN_EXISTED(HttpStatus.CONFLICT, "children existed")          // 409
 
     ;
 
